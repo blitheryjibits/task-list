d0cc340354890abb6e7c7614129ae75e1afac7a4
robert thornton <robert.thornton92@gmail.com>
2022-06-09 11:01:40 +0200

    Implemented ability to save projects and tasks to storage without directly accessing list.js or project.js

--- src/modules/DOMInterface.js
+++ src/modules/DOMInterface.js
@@ -1,4 +1,34 @@
-import { CreateProject } from "./modules/projects";
-import { CreateTask } from "./modules/task";
-import { CreateList } from './modules/list';
-import { storage } from "./modules/storage";
+import { CreateProject } from "./projects";
+import { CreateTask } from "./task";
+import { CreateList } from './list';
+import { storage } from "./storage";
+
+
+const UI = {
+
+    loadPage () {
+        localStorage.clear()
+        UI.load_projects();
+        
+
+    },
+    
+    load_projects() {
+        storage.addProject(CreateProject("new project"));
+        storage.addProject(CreateProject("project number 2"));
+        const _list = storage.getList();
+        const list = _list.getProjects();
+
+        // list[0].setTask(CreateTask("new task"));
+        // _list.setProject(list[0])
+        // //console.log(_list);
+        storage.saveTask(list[0],CreateTask("new task"));
+
+        console.log(localStorage);
+        // storage.saveList(_list)
+        // console.log(localStorage);
+    }
+ 
+}
+
+export { UI }
