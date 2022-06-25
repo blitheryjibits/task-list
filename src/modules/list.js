fecb1f6c0e5b32f20c1df2434fbed7d3c88edae9
robert thornton <robert.thornton92@gmail.com>
2022-06-25 16:14:33 +0200

    Added - major new functions. setCurrent(), getCurrent() updates metadat that signifies if a project is currently loaded. update_today(), update_week() were added to implement the logic for updating tasks in "today" and "this week" projects

--- src/modules/list.js
+++ src/modules/list.js
@@ -1,11 +1,22 @@
-import projectFactory from './projects'
+import { format, parseISO, isSameWeek } from 'date-fns';
+
 
 const List_proto = {
+
     getProjects () { return this.projects },
     
-    getProject (projectName) { 
-        return this.projects.find((project) => 
-        project.getName() === projectName) 
+    getProject (project) {
+        const name = typeof project === 'string' ? project : project.getName()
+        return this.projects.find((_project) => 
+        _project.getName() === name)
+    },
+
+    setCurrent (project) { 
+        this.getProjects().forEach( (item) => { if (item.getCurrent()) item.setCurrent()})
+        project.setCurrent()},
+    
+    getCurrent () {
+        return this.getProjects().find( (project) => project.getCurrent())
     },
     
     setProject (project) { 
@@ -26,8 +37,43 @@ const List_proto = {
         tempArray.forEach(project => this.projects.push(project));
     },
 
-    deleteProjects () { for (let i = this.projects.length-1; i >= 0; i--) this.projects.pop() }
+    deleteProjects () { for (let i = this.projects.length-1; i >= 0; i--) this.projects.pop() },
+
+
+    // Create new Date, get all projects and for each, 
+    // check today date and this week (mon -> sun) and month (1 -> last day)
+    // if in date, make copy and add to today and/or week projects and/or month
+    // use storage interface to save list in local storage
+    update_today () {
+        const today = this.getProject('Today')
+        const list = this.getProjects()
+        const date = format(new Date(), 'yyyy-MM-dd')
+        list.forEach(project => {
+            const tasks = project.getTasks()
+            tasks.forEach(task => {
+                if (task.getFormattedDate() === date) {
+                    today.replaceTask(task)
+                }
+            })
+        })
+        return today
+    },
+    update_week () {
+        const week = this.getProject('This Week')
+        const list = this.getProjects()
+        const date = format(new Date(), 'yyyy-MM-dd')
+        list.forEach(project => {
+            const tasks = project.getTasks()
+            tasks.forEach(task => {
+                if (isSameWeek(parseISO(task.getFormattedDate()), parseISO(date), {weekStartsOn: 1})) 
+                    week.replaceTask(task)
+            })
+        })
+        return week
+    }
 }
+
+
 
 const CreateList = () => {
     return Object.create(List_proto, {
